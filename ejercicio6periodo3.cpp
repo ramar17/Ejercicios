1d7976006d0fb84ef65ac7bc2eb4f965b696f5c9
ramar17 <112517021+ramar17@users.noreply.github.com>
2022-10-03 21:32:54 -0600

    Add files via upload

---
+++ ejercicio6periodo3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int numero;
+	do
+	{
+		cout << "Ingrese un numero ";
+		cin >> numero;
+	}
+	while(numero <= 100);
+	system("PAUSE");
+	return 0;
+}
